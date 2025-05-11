d304620dc3685d0c21f8df907a47e772668bbf0d
amirdp <amiradp3@gmail.com>
2025-05-11 15:49:01 +0330

    add models project and task: befor migrate

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Task
+
+
+@admin.register(Project)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner']
+
+    inlines = [
+        OrderItemInLine
+    ]
+
+
+@admin.register(Task)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'project', 'status', 'owner', ]
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+
+class Project(models.Model):
+
+    title = models.Charfield(max_lenght=200)
+    description = TextField(blank=True)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    TASK_STATUS = [
+        ('1', 'TODO'),
+        ('2', 'IN_PROGRESS'),
+        ('3', 'DONE')
+    ]
+
+    title = models.Charfield(max_lenght=200)
+    description = TextField(blank=True)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    status = models.CharField(max_length=10, choices=TASK_STATUS, blank=True, verbose_name='Whats is your status?')
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
